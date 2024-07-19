d426648b0d9c05c27800c5fb7b018ee3689e16b5
luzzdsg <123952294+luzzdsg@users.noreply.github.com>
2024-07-19 16:55:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 # 公众号配置
 # 公众号appId
-app_id = "wx******************"
+app_id = "wx65ce2c70d0eb7817"
 # 公众号appSecret
-app_secret = "f9a80260d33******************"
+app_secret = "554a886b911ef23c212d11910706a03c"
 # 模板消息id
 # 每日消息
-template_id1 = "wX-************************************"
+template_id1 = "YzQH5-L_1HzsuSG5GjDSsrbM-xaGQHre_s0khBjFX58"
 # 课程消息,上课提醒
 template_id2 = "loe1yHWr************************************"
 # 晚安心语
 template_id3 = "-fun9-2************************************"
 # 接收公众号消息的微信号
 # 这是openid
-user = ["ojhdR50T******************"]
+user = ["oQph06d2V1Y4eIY0bIUBsDiSd7xw"]
 
 # 信息配置
 # 所在省份
